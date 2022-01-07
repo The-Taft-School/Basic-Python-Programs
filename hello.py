4456df012bc1cac6fbabc11dd17e49236b3af8e3
Iyene Essien <iyenessien@iyenes-mbp.taftschool.org>
2022-01-07 12:56:18 -0500

    testing

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!?user/bin/python 
+a = float(input('Enter first side: '))
+b = float(input('Enter the first side: '))
+c = float(input('ENter the first side: '))
+
+s=(a+b+c)/2
+
+area = (s*(s-a)(s-b)(s-c)) ** 0.5
+
+print("the area of the triangle is: "%area)
